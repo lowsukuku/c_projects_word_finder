7e259ea32a409d785a976a9548874859f94a4752
lowsukuku_linux <lowsukuku_linux@lsk.com>
2020-03-30 11:22:23 +0300

    fixed tests

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,5 +1,6 @@
 #include <gtest/gtest.h>
 #include <stdio.h>
+#include <string>
 extern "C" {
 #include "../source/fileFinder.h"
 #include "../source/occurenciesFinder.h"
@@ -12,17 +13,25 @@ TEST(test, crashTest) {
 TEST(test, fileListTest) {
   namesVector names = {0, 0, 0};
   listdir((char *)"../../cpython", &names);
-  FILE *filelist = fopen((char *)"filelist.txt", (char *)"r");
+  FILE *filelist = fopen((char *)"../tests/filelist.txt", (char *)"r");
   char *line = (char *)malloc(1000);
   size_t nameCounter = 0;
   while (!feof(filelist)) {
-    ASSERT_NE(nameCounter, names.tailPosition);
-    fgets(line, 1000, filelist);
-    ASSERT_STREQ(line, names.data[nameCounter++]);
+    ASSERT_NE(nameCounter, names.tailPosition + 1);
+    if (fgets(line, 1000, filelist) == 0) {
+      fclose(filelist);
+      break;
+    }
+    std::string str = names.data[nameCounter++];
+    str += "\n";
+    ASSERT_STREQ(line, str.c_str());
   }
+  clearVector(&names);
+  free(line);
 }
 
 TEST(test, invalidPath) {
   namesVector names = {0, 0, 0};
   ASSERT_EQ(listdir((char *)"someRandomPath", &names), -1);
+  clearVector(&names);
 }
