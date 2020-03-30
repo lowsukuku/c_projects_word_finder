de5be87fd701ba105bce37da225cb34ecb50e8e5
lowsukuku_linux <lowsukuku_linux@lsk.com>
2020-03-30 09:03:30 +0300

    added test for file finder

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,9 +1,28 @@
 #include <gtest/gtest.h>
 #include <stdio.h>
 extern "C" {
+#include "../source/fileFinder.h"
 #include "../source/occurenciesFinder.h"
 }
 
-TEST(test, test1) {
+TEST(test, crashTest) {
   printSortedStatistics((char *)"../../cpython", (char *)"int");
+}
+
+TEST(test, fileListTest) {
+  namesVector names = {0};
+  listdir((char *)"../../cpython", &names);
+  FILE *filelist = fopen((char *)"filelist.txt", (char *)"r");
+  char *line = (char *)malloc(1000);
+  size_t nameCounter = 0;
+  while (!feof(filelist)) {
+    ASSERT_NE(nameCounter, names.tailPosition);
+    fgets(line, 1000, filelist);
+    ASSERT_STREQ(line, names.data[nameCounter++]);
+  }
+}
+
+TEST(test, invalidPath) {
+  namesVector names = {0};
+  ASSERT_EQ(listdir((char *)"someRandomPath", &names), -1);
 }
